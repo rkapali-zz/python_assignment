3ffcc9b04d9ac4f93e5cdc7816c4a25fff47dfe5
gambit <gambit@mint-gambit.com>
2014-12-22 14:36:34 +0545

    set1 and set2

---
+++ set1/pycat.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+import sys, zipfile
+#arg1 = str(sys.argv)
+#print arg1
+filename = str(sys.argv[1])
+if filename ends
+f = zipfile.ZipFile(filename,'r')
+content = f.read('')
+print content
+f.close()
